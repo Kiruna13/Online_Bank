167b76a255d1074de254d09153b4a147b026a49d
Kiruna13 <thomas.ducoulombier@free.fr>
2021-12-10 11:12:37 +0100

    Page connexion terminée

--- app/src/main/java/com/example/banqueenligne/Connexion.kt
+++ app/src/main/java/com/example/banqueenligne/Connexion.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
 
-class MainActivity : AppCompatActivity() {
+class Connexion : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
