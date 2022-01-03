d1455e4a68824c6d82714b8375062e237cd5ed00
Kiruna13 <thomas.ducoulombier@free.fr>
2022-01-03 14:00:55 +0100

    Page connexion terminée

--- app/src/main/java/com/example/banqueenligne/MakeVirement.kt
+++ app/src/main/java/com/example/banqueenligne/MakeVirement.kt
@@ -1,10 +1,11 @@
 package com.example.banqueenligne
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import androidx.appcompat.app.AppCompatActivity
 
-class Connexion : AppCompatActivity() {
+
+class MakeVirement : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
--- app/src/main/java/com/example/banqueenligne/Virement.kt
+++ app/src/main/java/com/example/banqueenligne/Virement.kt
@@ -1,4 +1,14 @@
 package com.example.banqueenligne
 
-class Virement {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Window
+
+class Virement : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        supportActionBar?.hide()
+        setContentView(R.layout.virement)
+    }
 }
