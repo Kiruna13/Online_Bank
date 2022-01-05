8f61a251475a531986d45c62c3527148919b3c81
lexraccoon <laura.goncalves1@epsi.fr>
2022-01-05 17:43:16 +0100

    commit version la plus complete

--- app/src/main/java/com/example/banqueenligne/DetailBeneficiaire.kt
+++ app/src/main/java/com/example/banqueenligne/DetailBeneficiaire.kt
@@ -1,15 +1,21 @@
 package com.example.banqueenligne
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.view.Window
+import android.widget.Button
 import com.google.android.flexbox.FlexboxLayout
 
-class DetailBeneficiaire : AppCompatActivity() {
+class DetailBeneficiaire : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         setContentView(R.layout.activity_detail_beneficiaire)
+
+        val b = findViewById<Button>(R.id.btn_virement)
+        b.setOnClickListener(this)
 
         //actionbar
         val actionbar = supportActionBar
@@ -18,5 +24,16 @@ class DetailBeneficiaire : AppCompatActivity() {
         //set back button
         actionbar.setDisplayHomeAsUpEnabled(true)
         actionbar.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    override fun onClick(view: View) {
+        when (view.getId()) {
+            R.id.btn_virement -> startActivity(Intent(this@DetailBeneficiaire, MakeVirement::class.java))
+        }
     }
 }
